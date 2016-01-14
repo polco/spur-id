b134b322ff0d73f4ddf8dcaabcbb2583e6d65c3d
Paul Colleoni <paul.colleoni@gmail.com>
2016-01-14 16:59:15 +0900

    first implementation

---
+++ index.js
@@ -0,0 +1,21 @@
+var ATTRIBUTE_NAME = 'data-spurid';
+var WINDOW_NODE_ID = 'window-node';
+var DOCUMENT_NODE_ID = 'document-node';
+
+var counter = 1;
+
+function getDOMNodeId(element, createIfNull) {
+  if (element === window) { return WINDOW_NODE_ID; }
+  if (element === document) { return DOCUMENT_NODE_ID; }
+  var id = element.getAttribute(ATTRIBUTE_NAME);
+  if (createIfNull && !id) {
+    id = counter;
+    counter += 1;
+    element.setAttribute(ATTRIBUTE_NAME, id);
+  }
+  return id;
+}
+
+module.exports = {
+  getDOMNodeId: getDOMNodeId
+};
